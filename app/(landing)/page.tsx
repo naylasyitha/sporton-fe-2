1a73032e59a6eefac8d667adc25d9bb22b949fa7
naylasyitha <naylasyitha30@gmail.com>
2026-01-07 13:32:42 +0700

    commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import CategoriesSection from "./components/home/categories";
+import HeroSection from "./components/home/hero";
+import ProductsSection from "./components/home/product";
+
+export default function Home() {
+  return <main>
+    <HeroSection />
+    <CategoriesSection />
+    <ProductsSection />
+  </main>
+}
