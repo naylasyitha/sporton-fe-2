db603778b39ec183c24b849739c1a65ad562e67f
naylasyitha <naylasyitha30@gmail.com>
2026-01-07 13:59:15 +0700

    fixing categories

--- app/(landing)/components/home/categories.tsx
+++ app/(landing)/components/home/categories.tsx
@@ -32,7 +32,7 @@ const categoryList = [
 const CategoriesSection = () => {
     return (
         <section id="category-section" className="container mx-auto pb-20">
-            <div className="flex justify-between">
+            <div className="flex justify-between mt-8">
                 <h2 className="font-bold text-2xl">Browse By Categories</h2>
                 <Link href="#" className="flex gap-2 text-primary font-medium">
                 <span className="self-center">See All Categories</span>
